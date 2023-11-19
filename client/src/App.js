b1fc3d72f1b45c70adba18efc92d7991d3946214
Michael Salerno <msalerno1993@gmail.com>
2023-11-18 21:21:13 -0500

    setup everything for NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,28 @@ import HomePage from "scenes/Home/HomePage";
 import LoginPage from "scenes/Login/LoginPage";
 import ProfilePage from "scenes/Profile/ProfilePage";
 import NavBar from "scenes/NavBar/NavBar";
+import { useMemo } from "react";
+import { UseSelector, useSelector } from "react-redux/";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "Theme";
 
 function App() {
+
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 
   return (
     <div className="app">
     <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
     <Routes>
       <Route path='/' element={<LoginPage />} />
       <Route path='/home' element={<HomePage />} />
       <Route path='/profile/:userId' element={<ProfilePage />} />
     </Routes>
+    </ThemeProvider>
     </BrowserRouter>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import authReducer from "./state/indexState.js";
+import authReducer from "./state/indexState";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
   persistStore,
   persistReducer,
   FLUSH,
-  HYDRATE,
+  REHYDRATE,
   PAUSE,
   PERSIST,
   PURGE,
@@ -22,20 +22,19 @@ const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => {
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoreActions: [FLUSH, HYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    });
-  },
+    }),
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
         <App />
       </PersistGate>
     </Provider>
