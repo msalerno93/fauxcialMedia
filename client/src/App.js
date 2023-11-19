4e741aa2543e37146962ff2c5bba115490fef776
Michael Salerno <msalerno1993@gmail.com>
2023-11-19 13:03:28 -0500

    navbar completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scenes/Home/HomePage";
-import LoginPage from "scenes/Login/LoginPage";
-import ProfilePage from "scenes/Profile/ProfilePage";
-import NavBar from "scenes/NavBar/NavBar";
+import HomePage from "scenes/Home/HomePage.jsx";
+import LoginPage from "scenes/Login/LoginPage.jsx";
+import ProfilePage from "./scenes/Profile/ProfilePage.jsx";
 import { useMemo } from "react";
-import { UseSelector, useSelector } from "react-redux/";
+import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "Theme";
+import { themeSettings } from "./Theme";
 
 function App() {
-
-  const mode = useSelector((state) => state.mode)
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
-    <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-    <Routes>
-      <Route path='/' element={<LoginPage />} />
-      <Route path='/home' element={<HomePage />} />
-      <Route path='/profile/:userId' element={<ProfilePage />} />
-    </Routes>
-    </ThemeProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={<HomePage />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={<ProfilePage/>}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/scenes/Home/HomePage.jsx
+++ client/src/scenes/Home/HomePage.jsx
@@ -1,10 +1,12 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import NavBar from 'scenes/NavBar/NavBar'
 
 const HomePage = () => {
   return (
-    <div>
-        HomePage
-    </div>
+    <Box>
+      <NavBar />
+    </Box>
   )
 }
 
